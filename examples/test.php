6bd132b3aa6bb05b614a6260e159bb403264521f
Alaa Attya <alaa.attya@tajawal.com>
2017-07-29 16:54:54 +0200

    init

--- examples/test.php
+++ examples/test.php
@@ -1,10 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: alaaattya
- * Date: 7/23/17
- * Time: 7:38 PM
- */
+
+require_once __DIR__ . './../vendor/autoload.php';
+use Tests\Models\Person;
+
 
 $transformer = new Transformer\BaseTransformStrategy();
 
@@ -12,5 +10,10 @@ $testArray = [
     'name' => 'alaa',
     'email' => 'alaa.attya91@gmail.com'
 ];
-$p = new Person();
-$transformer->hydrate($testArray, $p);
+
+// hydrate array to get an object
+$object = $transformer->hydrate($testArray, Person::class);
+var_dump($object);
+
+// Transform object to get its array
+print_r($transformer->transform($object));
